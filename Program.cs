62c2139d43ebf4c299741a1dbb53755654f21112
Abdulaziz Egamberdiyev <145344817+abdulaziz0413@users.noreply.github.com>
2024-02-09 20:23:24 +0500

    i learned and add some syntaxes

--- Program.cs
+++ Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(12.39816); //double
+            Console.WriteLine(1_200_000_000_000.39816m); //decimal
+            Console.WriteLine(12.39816f); //float
+            //----boolean----//
+            Console.WriteLine(true);
+            Console.WriteLine(false);
+
+            //-----var-----//
+            var message = "hello world";
+            Console.WriteLine(34.40M);
+
         }
     }
 }
