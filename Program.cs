1bfb202c790c5d32b08af077e4b8155544c4b9f7
Abdulaziz Egamberdiyev <145344817+abdulaziz0413@users.noreply.github.com>
2024-02-09 18:38:03 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _100daycoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
