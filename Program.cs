efa3433b5bc6594239442d97194fda79fecb2878
Abdulaziz Egamberdiyev <145344817+abdulaziz0413@users.noreply.github.com>
2024-03-08 16:57:57 +0500

    i added new codes

--- Program.cs
+++ Program.cs
@@ -14,6 +14,27 @@
             //-----var-----//
             var message = "hello world";
             Console.WriteLine(34.40M);
+            //-----string----//
+            string firstName = "Bob";
+            string message2 = $"Hello {firstName}!";
+            Console.WriteLine(message2);
+            string projectName = "First-Project";
+            Console.WriteLine($@"C:\Output\{projectName}\Data");
+            //-------math_operators-------//
+            int sum = 7 + 5;
+            int difference = 7 - 5;
+            int product = 7 * 5;
+            decimal quotient = 7 / 5.0m;
+
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Difference: " + difference);
+            Console.WriteLine("Product: " + product);
+            Console.WriteLine("Quotient: " + quotient);
+
+            int first = 7;
+            int second = 5;
+            decimal quotient1 = (decimal)first / (decimal)second;
+            Console.WriteLine(quotient1);
 
         }
     }
